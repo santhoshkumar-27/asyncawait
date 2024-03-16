71fe4e56013c23a72af66ad13ebf3512e71c8c1f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-16 22:13:36 +0530

    fix: without try catch block

--- asyn.js
+++ asyn.js
@@ -1,44 +1,54 @@
-async function handleAsyncOperations() {
-    const a = await step();
-    const b = await step(a);
-    const c = await step(b);
+function step(adsf = 1) {
+    return Promise.reject(adsf)
+}
 
+const handleError = (error) => console.log(error);
+
+// async function handleAsyncOperations() {
+//     const a = await step();
+//     const b = await step(a);
+//     const c = await step(b);
+
+//     return [a, b, c]
+// }
+
+// /**
+//  * @description Tower of terror
+//  */
+// async function handleAsyncOperationsOneMethod() {
+//     let a;
+//     let b;
+//     let c;
+    
+//     try {
+//         a = await step();
+//     } catch (e) {
+//         handleError(e)
+//     }
+
+//     try {
+//         b = await step(a);
+//     } catch (e) {
+//         handleError(e)
+//     }
+
+//     try {
+//         c = await step(b);
+//     } catch (e) {
+//         handleError(e)
+//     }
+
+//     return a + b + c;
+// }
+
+
+
+async function getSimpler() {
+    const a = await step().catch((reason) => handleError(reason));
+    const b = await step(a).catch((reason) => handleError(reason));
+    const c = await step(b).catch((reason) => handleError(reason));
+    console.log(a, b, c)
     return [a, b, c]
 }
 
-
-function step(adsf = 1) {
-    return Promise.resolve(adsf)
-}
-
-/**
- * @description Tower of terror
- */
-async function handleAsyncOperationsOneMethod() {
-    let a;
-    let b;
-    let c;
-    
-    try {
-        a = await step();
-    } catch (e) {
-        handleError(e)
-    }
-
-    try {
-        b = await step(a);
-    } catch (e) {
-        handleError(e)
-    }
-
-    try {
-        c = await step(b);
-    } catch (e) {
-        handleError(e)
-    }
-
-    return a + b + c;
-}
-
-
-const handleError = (error) => console.log(error);
+console.log(getSimpler())
