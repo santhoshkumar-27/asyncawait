1ead7e4dd172097b015067610cf7ec5df5749881
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-16 21:38:28 +0530

    with handling of errors

--- asyn.js
+++ asyn.js
@@ -11,3 +11,34 @@ function step(adsf = 1) {
     return Promise.resolve(adsf)
 }
 
+/**
+ * @description Tower of terror
+ */
+async function handleAsyncOperationsOneMethod() {
+    let a;
+    let b;
+    let c;
+    
+    try {
+        a = await step();
+    } catch (e) {
+        handleError(e)
+    }
+
+    try {
+        b = await step(a);
+    } catch (e) {
+        handleError(e)
+    }
+
+    try {
+        c = await step(b);
+    } catch (e) {
+        handleError(e)
+    }
+
+    return a + b + c;
+}
+
+
+const handleError = (error) => console.log(error);
