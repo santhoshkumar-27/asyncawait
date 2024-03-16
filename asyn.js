217f7155de211a1cd15fd9b5432fa5685d252a4b
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-16 22:29:08 +0530

    feat: handle with try catch block

--- asyn.js
+++ asyn.js
@@ -4,45 +4,43 @@ function step(adsf = 1) {
 
 const handleError = (error) => console.log(error);
 
-// async function handleAsyncOperations() {
-//     const a = await step();
-//     const b = await step(a);
-//     const c = await step(b);
-
-//     return [a, b, c]
-// }
-
-// /**
-//  * @description Tower of terror
-//  */
-// async function handleAsyncOperationsOneMethod() {
-//     let a;
-//     let b;
-//     let c;
+/**
+ * @description Tower of terror this function  handled the reject promise
+ * with try catch method it handled
+ */
+async function handleAsyncOperationsOneMethod() {
+    let a;
+    let b;
+    let c;
     
-//     try {
-//         a = await step();
-//     } catch (e) {
-//         handleError(e)
-//     }
+    try {
+        a = await step();
+    } catch (e) {
+        handleError(e)
+    }
 
-//     try {
-//         b = await step(a);
-//     } catch (e) {
-//         handleError(e)
-//     }
+    try {
+        b = await step(a);
+    } catch (e) {
+        handleError(e)
+    }
 
-//     try {
-//         c = await step(b);
-//     } catch (e) {
-//         handleError(e)
-//     }
+    try {
+        c = await step(b);
+    } catch (e) {
+        handleError(e)
+    }
+    console.log(a, b, c)
+    return a + b + c;
+}
 
-//     return a + b + c;
-// }
+console.log(handleAsyncOperationsOneMethod())
 
 
-
+/**
+ * @description with function, don't use the try catch block instead they handle by catch call back 
+ * functions 
+ */
 async function getSimpler() {
     const a = await step().catch((reason) => handleError(reason));
     const b = await step(a).catch((reason) => handleError(reason));
@@ -51,7 +49,7 @@ async function getSimpler() {
     return [a, b, c]
 }
 
-console.log(getSimpler())
+// console.log(getSimpler())
 
 /**
  * 
@@ -65,4 +63,4 @@ async function handleAsyncOperations() {
     console.log(a, b, c)
     return [a, b, c]
 }
-console.log(handleAsyncOperations())
+// console.log(handleAsyncOperations())
